e384aa5308ac65e7df68fd24c4d2254eba6796e0
Laaf <luizaaf5434@gmail.com>
2023-07-04 08:52:53 -0300

    atualizando main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from rich.console import Console
-# from rich.layout import Layout
-
-console = Console()
-
 options = {1: "Criar cluster", 2: "Destruir cluster",
            3: "Atualizar cluster", 4: "Links uteis", 5: "Sair"}
 
 while True:
-    console.print("VLE - for Kubernetes :anchor: \n",
-                  justify="center", style="blue")
+    print("VLE - for Kubernetes\n",)
 
     for k, v in options.items():
-        console.print(f"{k} - {v}", justify="center", style="blue")
+        print(f"{k} - {v}")
 
-    choice = input("Digite a opção desejada: ")
-    if int(choice) == 5:
-        console.print("Obrigado por usar o programa até uma proxima :smile:",
-                      justify="center", style="green")
+    option_choice = input("Digite a opção desejada: ")
+
+    if int(option_choice) == 1:
+        ...
+
+    if int(option_choice) == 5:
+        print("Obrigado por usar o programa até uma proxima")
         break
