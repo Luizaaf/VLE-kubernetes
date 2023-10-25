a19dda7f27b8c51217291280d3e9bd36933bdf5b
Laaf <luizaaf5434@gmail.com>
2023-10-24 21:22:56 -0300

    mudando invetorio

--- infraestrutura/main.tf
+++ infraestrutura/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }    
 }
@@ -18,14 +18,17 @@ locals {
   instance-type = "t2.medium"
   acess-key = "vockey"
   volume-size = 20
+  inventory-path = "../ansible/inventory/aws_ec2.yml"
+  key-path = "~/Downloads/labsuser.pem"
+  playbook-path = "../ansible/playbook.yml"
 }
 
 # Grupo de segurança
 
 resource "aws_security_group" "this" {
   name = "allow_kubernetes"
-  description = "group for allow kubernetes traffic"
-  vpc_id = "vpc-07a3b39a3e4228a8b "
+  description = "Group for allow kubernetes traffic"
+  vpc_id = "vpc-07a3b39a3e4228a8b"
 
   ingress {
     description = "Allow ssh traffic"
@@ -37,65 +40,18 @@ resource "aws_security_group" "this" {
   }
 
   ingress {
-    description = "Kubernetes API server"
-    from_port        = 6443
-    to_port          = 6443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description = "etcd server client API"
-    from_port        = 2379
-    to_port          = 2380
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description = "Kubelet API"
-    from_port        = 10250
-    to_port          = 10250
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description = "kube-scheduler"
-    from_port        = 10259
-    to_port          = 10259
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description = "kube-controller-manager"
-    from_port        = 10257
-    to_port          = 10257
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description = "NodePort Services"
-    from_port        = 30000
-    to_port          = 32767
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    description = "Allow all internal group traffic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+      description = "Allow all external traffic"
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
   }
 }
 
@@ -106,6 +62,7 @@ resource "aws_instance" "master" {
     instance_type = local.instance-type
     vpc_security_group_ids = [aws_security_group.this.id]
     key_name = local.acess-key
+    count = 1
 
     ebs_block_device {
       volume_size = local.volume-size
@@ -118,12 +75,13 @@ resource "aws_instance" "master" {
     }
 }
 
-resource "aws_instance" "worker-01" {
+resource "aws_instance" "workers" {
 
     ami = local.ami-id
     instance_type = local.instance-type
     vpc_security_group_ids = [aws_security_group.this.id]
     key_name = local.acess-key 
+    count = 2
 
     ebs_block_device{
       volume_size = local.volume-size
@@ -136,33 +94,16 @@ resource "aws_instance" "worker-01" {
     }
 }
 
-resource "aws_instance" "worker-02" {
-  
-    ami = local.ami-id
-    instance_type = local.instance-type
-    vpc_security_group_ids = [aws_security_group.this.id]
-    key_name = local.acess-key 
-
-    ebs_block_device{
-      volume_size = local.volume-size
-      device_name = "/dev/sda1"
-    }
-
-    tags = {
-      Name = "worker-02"
-    }
+resource "null_resource" "name" {  
 }
 
 # outputs
 
 output "instance-ip-master" {
-  value = aws_instance.master.public_ip
+  value = aws_instance.master[*].public_ip
 }
 
-output "instance-ip-worker01" {
-  value = aws_instance.worker-01.public_ip
+output "Workres-ip" {
+  value = aws_instance.workers[*].public_ip
 }
 
-output "instance-ip-worker02" {
-  value = aws_instance.worker-02.public_ip
-}
